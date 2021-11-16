977f97b5134a5841aef80667d741cd1f3256ec85
Abarnajo <94069323+Abarnajo@users.noreply.github.com>
2021-11-16 17:22:36 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,29 @@
+# string slicing
+a = " World Class"
+print(a[1:3])
+
+#Upper
+a = "world class"
+print(a.upper())
+
+#lower
+a = "WORLD CLASS"
+print(a.lower())
+
+#strip
+a = "World, class"
+print(a.strip())
+
+#Replace
+a = "world class"
+print(a.replace("w", "G"))
+
+#split
+a = "world, class"
+print(a.split(","))
+
+
+
+
+
+
